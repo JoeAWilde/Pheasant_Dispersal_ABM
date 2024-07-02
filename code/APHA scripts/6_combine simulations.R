d08b8707b97cd9c804e0190f3141387bf4138d60
Joe A. Wilde <70703036+JoeAWilde@users.noreply.github.com>
2024-07-02 13:01:11 +0100

    separating atlas and apha

--- code/APHA scripts/6_combine simulations.R
+++ code/APHA scripts/6_combine simulations.R
@@ -1,18 +1,26 @@
 library(tidyverse)
 library(progress)
 
-sim_files <- list.files("outputs/script_5/ATLAS output/")
-
-for(i in sim_files) {
-  if(i == sim_files[1]) pb <- progress_bar$new(total = length(sim_files), 
-                                               format = "[:bar] :percent eta::eta", 
-                                               clear = F); pb$tick(0)
-  df_id <- readRDS(paste0("outputs/script_5/ATLAS output/", i))
+for(ss in c("A", "B", "D")) {
   
-  if(i == sim_files[1]) {
-    all_df <- df_id
-  } else {
-    all_df <- rbind(all_df, df_id)
+  sim_files <- list.files(paste0("outputs/script_5/APHA output/site ", ss))
+  
+  for(i in sim_files) {
+    if(i == sim_files[1]) pb <- progress_bar$new(total = length(sim_files), 
+                                                 format = "[:bar] :percent eta::eta", 
+                                                 clear = F); pb$tick(0)
+    df_id <- readRDS(paste0("outputs/script_5/APHA output/site ", ss, "/", i))
+    
+    if(i == sim_files[1]) {
+      all_df <- df_id
+    } else {
+      all_df <- rbind(all_df, df_id)
+    }
+    pb$tick()
   }
-  pb$tick()
-};saveRDS(all_df, "outputs/script_6/simulation_data.rds")
+  if(ss == "A") {
+    all_sites_df <- all_df
+  } else {
+    all_sites_df <- rbind(all_sites_df, all_df)
+  }
+};saveRDS(all_sites_df, "outputs/script_6/APHA output/simulation_data.rds")
