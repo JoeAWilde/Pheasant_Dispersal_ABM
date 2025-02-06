e0e9525a431d9475b9814ffe4387ffeb12a905db
Joe A. Wilde <70703036+JoeAWilde@users.noreply.github.com>
2025-02-06 10:27:25 +0000

    update time limit for each run

--- code/PA sites/1_no_manage/As_site/As_run_sim.sh
+++ code/PA sites/1_no_manage/As_site/As_run_sim.sh
@@ -3,7 +3,7 @@
 #SBATCH --job-name="As_no_manage"  # Job name
 #SBATCH --output=job_output_%j.txt                # Output file (%j = job ID)
 #SBATCH --error=job_error_%j.txt                  # Error file
-#SBATCH --time=17:00:00                           # Time limit (hh:mm:ss)
+#SBATCH --time=85:00:00                           # Time limit (hh:mm:ss)
 #SBATCH --ntasks=1                                # Number of tasks (1 for running a single script)
 #SBATCH --cpus-per-task=25                         # Number of CPUs per task
 #SBATCH --mem=32G                                 # Memory per node
