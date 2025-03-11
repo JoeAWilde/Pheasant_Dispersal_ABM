cb0a6cd5fed8164d46a39a43c6eb0b22892e6f05
JoeAWilde <j.wilde@hotmail.co.uk>
2025-03-11 13:55:02 +0000

    update .sh files for 3_

--- code/PA sites/3_baseline/array_job/Du_site/Du_array.sh
+++ code/PA sites/3_baseline/array_job/Du_site/Du_array.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH --job-name="Du_array"           # Job name
+#SBATCH --job-name="Du_3_500"           # Job name
 #SBATCH --output=job_output_%A_%a.txt   # Output file (%A = job array ID, %a = task ID)
 #SBATCH --error=job_error_%A_%a.txt     # Error file
 #SBATCH --time=96:00:00                 # Time limit
